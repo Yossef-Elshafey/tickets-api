c1503bd131a3d8568dee553a9892119169e0c508
Yossef-Elshafey <yssef0472@gmail.com>
2024-06-01 20:50:47 +0300

    fix reservation object permission

--- reservation/views.py
+++ reservation/views.py
@@ -6,19 +6,11 @@ from reservation.serializers import ReservationSer
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return False
-
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS and obj.customer == request.user:
-            return True
-        return False
+        return obj.customer == request.user
 
 
 class ListReservation(generics.ListCreateAPIView):
-    # ay 7ad y2dr y3ml post bas lma y3ml get hygelo 7agto bas
     permission_classes = [permissions.IsAuthenticated]
     queryset = Reservation.objects.prefetch_related("movie_id", "customer").all()
     serializer_class = ReservationSer
@@ -36,10 +28,7 @@ class SingleReservation(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ReservationSer
     lookup_field = "id"
 
-    def get(self, request, *args, **kwargs):
-        user = self.request.user
-        queryset = self.get_queryset().filter(
-            Q(customer=self.request.user.id) & Q(id=self.kwargs["id"])
-        )
-        serailizer = self.get_serializer(queryset, many=True)
-        return Response(serailizer.data)
+    def get_object(self):
+        obj = generics.get_object_or_404(self.get_queryset(), id=self.kwargs["id"])
+        self.check_object_permissions(self.request, obj)
+        return obj
